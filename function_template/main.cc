545bb9bf5e44a3271846beeebf37fe8bf649a63c
petru <smart__petea@mail.ru>
2015-10-27 10:16:29 +0200

    function with property

--- function_template/main.cc
+++ function_template/main.cc
@@ -1,8 +1,6 @@
 #include <nan.h>
 
 /*
- * Nan::New<v8::FunctionTemplate>() called without arguments will be a template for a JS function of form 
- ---JS---
  var template = function() {}
  */
 void SetEmptyTemplate(v8::Local<v8::Object>& exports) 
@@ -11,9 +9,22 @@ void SetEmptyTemplate(v8::Local<v8::Object>& exports)
     exports->Set(Nan::New("emptyTemplate").ToLocalChecked(), emptyTemplate->GetFunction());
 }
 
+/*
+ var template = function() {}
+ template.MyProperty = 5
+ */
+void SetTemplateWithProperty(v8::Local<v8::Object>& exports)
+{
+    v8::Local<v8::FunctionTemplate> tmpl = Nan::New<v8::FunctionTemplate>();
+    tmpl->Set(Nan::New("MyProperty").ToLocalChecked(), Nan::New(5));
+
+    exports->Set(Nan::New("propertyTemplate").ToLocalChecked(), tmpl->GetFunction());
+}
+
 void Init(v8::Local<v8::Object> exports)
 {
     SetEmptyTemplate(exports);
+    SetTemplateWithProperty(exports);
 }
 
 NODE_MODULE(fTemplate, Init)
