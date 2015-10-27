2b7f6720c7c451a7570d7951ed05dd248d1cec8d
petru <smart__petea@mail.ru>
2015-10-27 11:07:20 +0200

    template with prototype

--- function_template/main.cc
+++ function_template/main.cc
@@ -16,15 +16,35 @@ void SetEmptyTemplate(v8::Local<v8::Object>& exports)
 void SetTemplateWithProperty(v8::Local<v8::Object>& exports)
 {
     v8::Local<v8::FunctionTemplate> tmpl = Nan::New<v8::FunctionTemplate>();
-    tmpl->Set(Nan::New("MyProperty").ToLocalChecked(), Nan::New(5));
+    Nan::SetTemplate(tmpl, "MyProperty", Nan::New(5));
 
     exports->Set(Nan::New("propertyTemplate").ToLocalChecked(), tmpl->GetFunction());
+}
+
+/*
+ var template = function() {}
+ template.prototype.proto_method = function() {}
+ template.prototype.proto_const = 100
+ */
+void SetTemplateWithPrototype(v8::Local<v8::Object>& exports)
+{
+    //var template = function() {}
+    v8::Local<v8::FunctionTemplate> tmpl = Nan::New<v8::FunctionTemplate>(); 
+
+    //template.proto_method = function() {}
+    Nan::SetPrototypeTemplate(tmpl, "proto_method", Nan::New<v8::FunctionTemplate>()->GetFunction());
+
+    //template.proto_const = 2
+    Nan::SetPrototypeTemplate(tmpl, "proto_const", Nan::New(2));
+
+    exports->Set(Nan::New("prototypeTemplate").ToLocalChecked(), tmpl->GetFunction());
 }
 
 void Init(v8::Local<v8::Object> exports)
 {
     SetEmptyTemplate(exports);
     SetTemplateWithProperty(exports);
+    SetTemplateWithPrototype(exports);
 }
 
 NODE_MODULE(fTemplate, Init)
